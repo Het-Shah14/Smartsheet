29bff83cfd18de125d15138ff877cb3855106e6e
Het-Shah14 <het@airoxa.in>
2024-02-29 19:37:11 +0530

    authentication client

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const ClientID = "vqaotm0lkq2jntsnzbj";
+const ClientSecretKey = "p0566sjoypej99gjtaz";
+
+
 app.get('/',(req,res) => {
     res.send("Hello There !");
 });
@@ -9,3 +13,23 @@ app.get('/',(req,res) => {
 app.listen(3000,()=>{
     console.log("server is running at http://localhost:3000")
 })
+
+const SmartsheetAuthUrl = "https://app.smartsheet.com/b/authorize";
+const SmartsheetTokenUrl = "https://app.smartsheet.com/b/authorize";
+
+app.get("/login", (req, res) => {
+const redirectUrl = "http://localhost:3000/callback"
+const authorizationUrl = SmartsheetAuthUrl;
+    res.redirect(authorizationUrl);
+  });
+
+const config = {
+    ACCESS_SCOPE : 'READ_USERS',
+};
+
+  const authorizationURL = authorizationUrl({
+    client_id : ClientID,
+    response_type : 'code',
+    scope : config.ACCESS_SCOPE,
+  });
+
